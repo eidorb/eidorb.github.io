146d1e7a2fb384dbdf97b5d40d8a34d29a70dd92
eidorb <brodieface@gmail.com>
2025-02-26 15:46:36 +1000

    - add clockblockchain

--- epeen.py
+++ epeen.py
@@ -7,15 +7,30 @@ Edit: uv run marimo edit epeen.py
 import marimo
 
 __generated_with = "0.11.8"
-app = marimo.App(width="medium")
+app = marimo.App(width="full")
 
 
 @app.cell
 def _():
     import marimo as mo
+    import requests
 
     stack = mo.ui.run_button(label="stack")
     login = mo.ui.text(value="pertertodd", placeholder="GitHub login")
+
+    clockblockchain = []
+    return clockblockchain, login, mo, requests, stack
+
+
+@app.cell
+def _(clockblockchain, login, mo, requests, stack):
+    rate_limit = requests.get("http://api.github.com/rate_limit").json()["rate"]
+
+    if stack.value:
+        requests.get(
+            f"https://api.github.com/users/{login.value}"
+        )  # remove block from warehouse/burn request
+        clockblockchain.append(login.value)
 
     mo.md(
         """
@@ -30,14 +45,14 @@ def _():
 
         Mined blocks are distributed to warehouses at the precise rate of **one block per minute**.
 
-        Work on your task with strong determination:
+        Work on your task with one-pointed strong determination:
 
-        - {stack} block from warehouse onto blockchain
-
+        - stack block from warehouse onto blockchain
+        
         /// admonition | Calling all masterpiece-making Monets! 
 
         Starting today, our valued employees can customise blocks with a design that’s as unique as your login:
-    
+
         {login}
 
         Unleash your inner artist, superstars!  
@@ -45,7 +60,19 @@ def _():
         \- _Your_ Employee Experience team (aka your biggest fans forever!) ✨
         ///
 
-        #### What are employees saying about our workplace?
+        ## Warehouse
+
+        Capacity: {rate_limit["limit"]} 
+
+        Blocks: {rate_limit["remaining"]}
+
+        ## Clockblockchain
+
+        {mo.tree(clockblockchain)}
+
+        {stack} block from warehouse onto blockchain
+    
+        ## What are employees saying about our workplace?
 
         > "🫡 stack block from warehouse onto blockchain"
 
@@ -59,69 +86,13 @@ def _():
 
         \- Olivia, NY
         """
-    )
-    return login, mo, stack
+    ).center()
+    return (rate_limit,)
 
 
 @app.cell
-def _(mo):
-    from github import Github
-
-    gh = Github()
-    refresh = mo.ui.refresh(label="Refresh", options=["1s", "5s", "10s", "30s"])
-    consume = mo.ui.run_button(kind="danger", label="Consume")
-    resource_units = mo.ui.number(value=1)
-
-    mo.md(
-        f"""
-        {refresh} resource status
-
-        {consume} {resource_units} resource units
-        """
-    )
-    return Github, consume, gh, refresh, resource_units
-
-
-@app.cell
-def _(gh, mo, refresh):
-    import json
-    from datetime import datetime, timezone
-
-    if refresh.value:
-        sent = datetime.now(tz=timezone.utc)
-        rate_limit = gh.get_rate_limit()
-        received = datetime.utcnow().replace(tzinfo=timezone.utc)
-
-    mo.md(
-        f"""
-        request sent at
-
-        {sent}
-
-        rate limit status
-
-        limit: {rate_limit.core.limit}
-
-        remaining: {rate_limit.core.remaining}
-
-        used: {rate_limit.core.used}
-
-        reset: {str(rate_limit.core.reset.astimezone().replace(tzinfo=None))}
-
-        time until reset: {rate_limit.core.reset - received}
-
-        response received at
-
-        {received}
-
-        {type(rate_limit)}ack
-
-        {rate_limit.raw_headers}
-
-
-        """
-    ) if refresh.value else None
-    return datetime, json, rate_limit, received, sent, timezone
+def _():
+    return
 
 
 @app.cell
@@ -140,6 +111,12 @@ def _(mo):
         No designs that’d make HR blush or send the office into a tizzy—think workplace wow, not NSFW oof!
         """
     )
+    return
+
+
+@app.cell
+def _(clockblockchain):
+    clockblockchain
     return
 
 
